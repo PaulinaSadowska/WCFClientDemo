48b167fa773d17f36431f5b89987a2137bf09278
AraszDev <rafal.araszkiewicz@student.put.poznan.pl>
2016-10-10 21:55:06 +0200

    empty console application

---
+++ WCFClientDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
